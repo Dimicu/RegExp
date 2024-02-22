0f3f89f3abe25ce06349e5c590be01292dc2c70d
Dimicu <136081184+Dimicu@users.noreply.github.com>
2024-02-22 12:56:31 +0100

    Add files via upload

--- usa.js
+++ usa.js
@@ -1,41 +1,190 @@
-/*    • Fecha de creación: obligatorio y con formato dd/mm/aaaa.
-    • Empleado: será un nombre en clave formado por dos letras en mayúscula, un símbolo
-     y cuatro dígitos (ej. WW$1234)
-    • Destinatario: estará formado por dos o tres letras mayúsculas correspondientes al estado, 
-    un guión bajo, el nombre de la ciudad en minúsculas, dos puntos, y el código de distrito de 4 
-    digitos (ej. NM_NewYork:1234).
-    • Gramos: será un número del 100 al 5000.
-    • Composición: estará formado por una cantidad en gramos seguida de dos conjuntos de una o 
-    dos letras seguidas o no de un número. (ej. 200gC3OH7) 
-    • Número de cuenta de EEUU: supongamos que un número de cuenta estadounidense tiene 
-    el siguiente formato:
-
-    • Dos letras mayúsculas (no se incluye la Ñ) (A=0,B=1,C=2…).
-    • Dos dígitos: debe corresponderse con la suma de la primera letra y la segunda:
-     en caso de que sea menor que 10 se pone el 0 delante.
-    • Un guión.
-    • Doce dígitos de cuenta.
-    • Un guión.
-    • Dos dígitos de control
-    • Si el número está correcto se colocará en un campo de texto al lado del anterior,
-     pero sin guiones: solamente los números y las letras. */
-
-
-function validarDatos() {
-    let fecha = document.getElementsByTagName("fec")
-    let empleado = document.getElementsByTagName("emp")
-    let destinatario = document.getElementsByTagName("des")
-    let gramos = document.getElementsByTagName("grs")
-    let composicion = document.getElementsByTagName("comp")
-    let cta = document.getElementsByTagName("cta")
-    console.log(fecha);
+class Envio {
+    constructor(fec, emp, cli, gr, comp, cta) {
+        this.fecha = fec;
+        this.empleado = emp;
+        this.cliente = cli;
+        this.gramos = gr;
+        this.composicion = comp;
+        this.cuentaUsa = cta;
+    }
 }
 
 
-regFecha = /\d{2}\/\d{2}\/\d{4}/;
-regEmp = /[A-Z]{2}\W{1}\d{4}/;
-regDest = /^[A-Z]{2,3}_[a-zA-Z]+\:\d{4}/;
-regGram = /[1-4][0-9]{3}|5000/;
-regComp = /^[0-9]{1,4}g([A-Z]{1,2}[0-9]?){2}/;
-regNumCuenta = /[A-NO-Z]{2}[0-9]{2}\-[0-9]{12}\-[0-9]{2}/;
+/**--------------------------------------------------------------------------------TOMAR LOS DATOS -------------------------------------------------------------------*/
+
+function tomarDatos() {
+    let datos = document.getElementsByTagName("input");
+
+    for (let i = 0; i < datos.length; i++) {
+        //console.log(datos[i].value);
+    }
+    return datos;
+}
+
+/**-------------------------------------------------------------------------------VARIABLES GLOBALES------------------------------------------------------------------*/
+
+let coleccionDatos = tomarDatos();
+let arrayEnvios = [];
+
+/**--------------------------------------------------------------------------------VALIDAR LOS DATOS -------------------------------------------------------------------*/
+
+function validarFecha(elementos) {
+    let fecha = elementos["fec"].value;
+    let regFecha = /\d{2}\/\d{2}\/\d{4}/;
+    let mensaje = "";
+    let validacion = false;
+    if (regFecha.test(fecha)) {
+        mensaje = "Fecha Correcta";
+        validacion = true;
+    } else {
+        mensaje = "Error al introducir campo fecha" + "\n" +
+            "Compruebe que usa el formato dd/mm/aaaa";
+    }
+    console.log(mensaje);
+    return validacion;
+}
+
+function validarEmpleado(elementos) {
+    let emp = elementos["emp"].value;
+    let regEmp = /[A-Z]{2}\W{1}\d{4}/;
+    let mensaje = "";
+    let validacion = false;
+    if (regEmp.test(emp)) {
+        mensaje = "Nº Empleado Correcto";
+        validacion = true;
+    } else {
+        mensaje = "El numero de empleado no es correcto" +
+            "Introduzca un numero siguiendo el ejemplo AA$1234";
+    }
+    console.log(mensaje);
+    return validacion;
+}
+
+function validarCliente(elementos) {
+    let cli = elementos["cli"].value;
+    let regDest = /^[A-Z]{2,3}_[a-zA-Z]+\:\d{4}/;
+    let mensaje = "";
+    let validacion = false;
+    if (regDest.test(cli)) {
+        mensaje = "Nº Cliente Correcto";
+        validacion = true;
+    } else {
+        mensaje = "Error en Nº Cliente: Siga el ejemplo NY_NewYork:1234"
+    }
+    console.log(mensaje);
+    return validacion;
+}
+
+function validarGramos(elementos) {
+    let g = elementos["grs"].value;
+    let regGram = /(?=[1-4][0-9]{3})|5000/; //ARREGLADO ANTES ESTABA let regGram = /[1-4][0-9]{3}|5000/ Y TRAGABA TODO QUE FUERA MAS DE 4 CIFRAS
+    let mensaje = "";
+    let validacion = false;
+    if (regGram.test(g)) {
+        mensaje = "Nº gramos correcto";
+        validacion = true;
+    }
+    else {
+        mensaje = "Introduzca un peso entre 1000 y 5000 gramos";
+    }
+    console.log(mensaje);
+    return validacion;
+}
+
+
+
+function validarComposicion(elementos) {
+    let comp = elementos["comp"].value;
+    let regComp = /^[0-9]{1,4}g([A-Z]{1,2}[0-9]?){2}/;
+    let mensaje = "";
+    let validacion = false;
+
+    if (regComp.test(comp)) {
+        mensaje = "La composicion quimica es correcta";
+        validacion = true;
+    } else {
+        mensaje = "Introduzca una composicion quimica adecuada: ej.: 200gC3OH7";
+    }
+    console.log(mensaje);
+    return validacion;
+
+}
+
+function validarCuenta(elementos) {
+    let cta = elementos["cta"].value;
+    let abc = "ABCDEFGHIJKLMNOPKRSTUVWXYZ";
+    let numA = abc.indexOf(cta[0]);
+    let numB = abc.indexOf(cta[1]);
+    let mensaje = "";
+    let validacion = false;
+    let sumLetras = numA + numB;
+
+    let digitos = parseInt(cta.substring(2, 4));
+
+    if (sumLetras == digitos) {
+        mensaje = "Los digitos son correctos";
+        validacion = true;
+
+    } else {
+        console.log("Los digitos no coinciden con las letras: Introduzca un Nº Cuenta valido")
+    }
+    console.log(mensaje);
+    return validacion;
+}
+
+
+
+/**-------------------------------------------------------------------------------EJECUCION -------------------------------------------------------------------*/
+
+function validar() {
+    validarFecha(coleccionDatos);
+    validarEmpleado(coleccionDatos);
+    validarCliente(coleccionDatos);
+    validarGramos(coleccionDatos);
+    validarComposicion(coleccionDatos);
+    validarCuenta(coleccionDatos);
+
+}
+function crearEnvio(valor1, valor2, valor3, valor4, valor5, valor6) {
+
+    if (valor1 && valor2 && valor3 && valor4 && valor5 && valor6) {
+
+        let fecha = coleccionDatos["fec"];
+        let empleado = coleccionDatos["emp"];
+        let cliente = coleccionDatos["cli"];
+        let gramos = coleccionDatos["grs"];
+        let composicion = coleccionDatos["comp"];
+        let cuentaUsa = coleccionDatos["cta"];
+
+        let envio = new Envio(fecha, empleado, cliente, gramos, composicion, cuentaUsa);
+
+        arrayEnvios.push(envio);
+    }
+}
+
+function registrar() {
+    crearEnvio(validarFecha(coleccionDatos), validarEmpleado(coleccionDatos), validarCliente(coleccionDatos), validarGramos(coleccionDatos), validarComposicion(coleccionDatos), validarCuenta(coleccionDatos))
+}
+
+
+function limpiarCampos() {
+    for (let i = 0; i < coleccionDatos.length; i++) {
+        coleccionDatos[i] = "";
+
+    }
+}
+
+
+/**--------------------------------------------------------------------------------EXPRESIONES REGULARES-------------------------------------------------------------------*/
+/*
+
+
+let regFecha = /\d{2}\/\d{2}\/\d{4}/;
+let regEmp = /[A-Z]{2}\W{1}\d{4}/;
+let regDest = /^[A-Z]{2,3}_[a-zA-Z]+\:\d{4}/;
+let regGram = /[1-4][0-9]{3}|5000/;
+let regComp = /^[0-9]{1,4}g([A-Z]{1,2}[0-9]?){2}/;
+let regNumCuenta = /[A-NO-Z]{2}[0-9]{2}\-[0-9]{12}\-[0-9]{2}/;
+
+*/
 
